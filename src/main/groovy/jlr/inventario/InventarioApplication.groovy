e3d18c6248e703d7fe0a62568732e0d98ee568ad
Aelwin <juanlopez633@gmail.com>
2021-11-11 18:19:36 +0100

    commit inicial

---
+++ src/main/groovy/jlr/inventario/InventarioApplication.groovy
@@ -0,0 +1,13 @@
+package jlr.inventario
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class InventarioApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(InventarioApplication, args)
+	}
+
+}
