c8535627f819ceddf49f4dfa8dcf35e9639b069a
Aelwin <juanlopez633@gmail.com>
2021-11-13 10:54:40 +0100

    anotacion @Repository

--- src/main/groovy/jlr/inventario/repository/AutorRepository.groovy
+++ src/main/groovy/jlr/inventario/repository/AutorRepository.groovy
@@ -2,7 +2,9 @@ package jlr.inventario.repository
 
 import jlr.inventario.model.Autor
 import org.springframework.data.repository.CrudRepository
+import org.springframework.stereotype.Repository
 
+@Repository
 interface AutorRepository extends CrudRepository<Autor, Long> {
 
 }
--- src/main/groovy/jlr/inventario/repository/LibroRepository.groovy
+++ src/main/groovy/jlr/inventario/repository/LibroRepository.groovy
@@ -2,7 +2,9 @@ package jlr.inventario.repository
 
 import jlr.inventario.model.Libro
 import org.springframework.data.repository.CrudRepository
+import org.springframework.stereotype.Repository
 
+@Repository
 interface LibroRepository extends CrudRepository<Libro, Long> {
 
 }
